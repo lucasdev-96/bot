a7cb915c7ec262d42571a4ec0cc1927fd1aa6455
Lucas <lucasgodoidev96@gmail.com>
2023-02-27 18:10:38 -0300

    feat: :sparkles: Create Validator and Interface

---
+++ app/Validators/NumberOfBenefitValidator.ts
@@ -0,0 +1,48 @@
+import { rules, schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class NumberOfBenefitValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  /*
+   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
+   *
+   * For example:
+   * 1. The username must be of data type string. But then also, it should
+   *    not contain special characters or numbers.
+   *    ```
+   *     schema.string({}, [ rules.alpha() ])
+   *    ```
+   *
+   * 2. The email must be of data type string, formatted as a valid
+   *    email. But also, not used by any other user.
+   *    ```
+   *     schema.string({}, [
+   *       rules.email(),
+   *       rules.unique({ table: 'users', column: 'email' }),
+   *     ])
+   *    ```
+   */
+  public schema = schema.create({
+    login: schema.string(),
+    password: schema.string(),
+    cpf: schema.string({}, [rules.regex(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)]),
+  })
+
+  /**
+   * Custom messages for validation failures. You can make use of dot notation `(.)`
+   * for targeting nested fields and array expressions `(*)` for targeting all
+   * children of an array. For example:
+   *
+   * {
+   *   'profile.username.required': 'Username is required',
+   *   'scores.*.number': 'Define scores as valid numbers'
+   * }
+   *
+   */
+  public messages: CustomMessages = {
+    required: '{{ field }} is required',
+    'cpf.regex': "Invalid Format"
+
+  }
+}
---
+++ app/interfaces/numberOfBenefitsValidatorInterface.ts
@@ -0,0 +1,5 @@
+export default interface  INumberOfBenefitsValidatorInterface{
+  login: string
+  password: string
+  cpf: string
+}
